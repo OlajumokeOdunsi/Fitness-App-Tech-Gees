6600ce476a10aabe1bb02478266698a32e5b7c85
Jum-jum-maker <80886533+OlajumokeOdunsi@users.noreply.github.com>
2024-05-02 00:10:57 +0100

    footer-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Main from './components/pages/Main'
+import Footer from './components/pages/Footer'
 function App() {
 
   return (
     <>
     <Main/>
+    <Footer/>
     </>
   )
 }
