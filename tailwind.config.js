7f723b32a2935d526bf3ff45b285e4831d83f592
tosvic <victoruhuo@yahoo.com>
2024-05-02 22:21:55 +0100

    little modifications

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      colors: {
+        'bg-color': '#BFC6CF',
+        'text-color': '#011639',
+        'logo-text': '#2A2C30',
+        'logo-color': '#137AF7',
+        'shadow-color': 'rgba(0, 0, 0, 0.308)',
+        'button': '#',
+      },
+
+    },
+  },
+  plugins: [],
+}
