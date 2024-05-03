cf27067eeefdff3ccd1bc68f2116ec7b82a399a3
Lloyd-Badeji <adetomisoye@gmail.com>
2024-05-03 10:57:39 +0100

    footer

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
       </div>
       
       <div className="footer-bottom">
-        <p>&copy; 2024 Your Website Name. All rights reserved.</p>
+        <p>&copy; 2024 The Vitality. All rights reserved.</p>
       </div>
     </div>
   );
