8d0f116054616d47e415ab2b526f283710abd1e4
Jum-jum-maker <80886533+OlajumokeOdunsi@users.noreply.github.com>
2024-05-03 12:44:38 +0100

    added-dashboard-footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src//*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+        'bg-color': '#BFC6CF',
+        'text-color': '#011639',
+        'logo-text': '#2A2C30',
+        'logo-color': '#137AF7',
+        'shadow-color': 'rgba(0, 0, 0, 0.308)',
+        'button': '#',
+        // 'img': "linear-gradient(to right bottom, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url('public/05.jpg')",
+      },
+
+    },
   },
-  plugins: [],
+  plugins: [],
 }
+
 
