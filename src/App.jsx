3c85d3cdb381393877e917bb36265814d52b0cf1
Jum-jum-maker <80886533+OlajumokeOdunsi@users.noreply.github.com>
2024-05-04 14:19:52 +0100

    jumoke/update

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import Footer from './pages/footer/Footer'
 import LandingNav from './pages/nav/LandingNav'
 import Main from './pages/herosection/Main'
 import Footerdashboard from './dashboard/footerDashboard/Footerdashboard'
+import Show from './show/Show'
 
 
 function App() {
 
   return (
     <>
-    {/* <LandingNav/>
-    <Main/>
-    <Footer/> */}
+    <Show/>
     <Footerdashboard/>
 
     </>
