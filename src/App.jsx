753ab805474688d767cb4bd1837cfff82e2449ac
Jum-jum-maker <80886533+OlajumokeOdunsi@users.noreply.github.com>
2024-05-04 07:29:30 +0100

    the show folder is the one hlding all the componenets, then exported to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css'
-import Footer from './pages/footer/Footer'
-import LandingNav from './pages/nav/LandingNav'
-import Main from './pages/herosection/Main'
-
+import Show from './show/Show'
 function App() {
 
   return (
     <>
-    <LandingNav/>
-    <Main/>
-    <Footer/>
+    <Show/>
+
     </>
   )
 }
